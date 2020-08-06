4394aaa26aeefac1968c0d04fb5ec339f18add53
deevlas <deevlas@icloud.com>
2020-08-06 16:16:41 +0300

    finish HW

--- script.js
+++ script.js
@@ -53,45 +53,45 @@
 //  Добавьте каждому комментарию по кнопке для сворачивания его body.
 
 
-let lists = [{
-    titleOne: "Это название страницы",
-    body: "Это тело страницы"
-}, {
-    titleTwo: "Это название страницы",
-    body: "Это тело страницы"
-}, {
-    titleThree: "Это название страницы",
-    body: "Это тело страницы"
-}, {
-    titleFour: "Это название страницы",
-    body: "Это тело страницы"
-}]
+// let lists = [{
+//     titleOne: "Это название страницы",
+//     body: "Это тело страницы"
+// }, {
+//     titleTwo: "Это название страницы",
+//     body: "Это тело страницы"
+// }, {
+//     titleThree: "Это название страницы",
+//     body: "Это тело страницы"
+// }, {
+//     titleFour: "Это название страницы",
+//     body: "Это тело страницы"
+// }]
 
-let blockDiv = document.createElement('div')
-document.body.appendChild(blockDiv)
-blockDiv.setAttribute('id', 'titles')
+// let blockDiv = document.createElement('div')
+// document.body.appendChild(blockDiv)
+// blockDiv.setAttribute('id', 'titles')
 
-for (const list of lists) {
-    let blockList = document.createElement('div')
-    let listH2 = document.createElement('h2')
-    let secondListDiv = document.createElement('div')
-    let button = document.createElement('button')
+// for (const list of lists) {
+//     let blockList = document.createElement('div')
+//     let listH2 = document.createElement('h2')
+//     let secondListDiv = document.createElement('div')
+//     let button = document.createElement('button')
 
-    listH2.innerHTML = `${Object.keys(list)[0]}`
-    secondListDiv.innerHTML = `${list.body}`
-    button.classList.add('btn')
-    button.innerHTML = "Click me"
+//     listH2.innerHTML = `${Object.keys(list)[0]}`
+//     secondListDiv.innerHTML = `${list.body}`
+//     button.classList.add('btn')
+//     button.innerHTML = "Click me"
 
-    blockDiv.appendChild(blockList)
-    blockList.appendChild(listH2)
-    blockList.appendChild(secondListDiv)
-    blockList.appendChild(button)
+//     blockDiv.appendChild(blockList)
+//     blockList.appendChild(listH2)
+//     blockList.appendChild(secondListDiv)
+//     blockList.appendChild(button)
 
-    button.addEventListener("click", function () {
-        listH2.classList.toggle('item')
-        secondListDiv.classList.toggle('item')
-    })
-}
+//     button.addEventListener("click", function () {
+//         listH2.classList.toggle('item')
+//         secondListDiv.classList.toggle('item')
+//     })
+// }
 
 // - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
 // Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
@@ -254,21 +254,173 @@ btnCheckInSentence.addEventListener('click', function () {
 
 
 // -- взять массив пользователей
-// let usersWithAddress = [
-//                 {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//                 {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//                 {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//                 {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//                 {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//                 {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//                 {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//                 {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//                 {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//                 {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-//             ];
+let usersWithAddress = [{
+        id: 1,
+        name: 'vasya',
+        age: 31,
+        status: false,
+        address: {
+            city: 'Lviv',
+            street: 'Shevchenko',
+            number: 16
+        }
+    },
+    {
+        id: 2,
+        name: 'petya',
+        age: 30,
+        status: true,
+        address: {
+            city: 'Kyiv',
+            street: 'Shevchenko',
+            number: 1
+        }
+    },
+    {
+        id: 3,
+        name: 'kolya',
+        age: 29,
+        status: true,
+        address: {
+            city: 'Lviv',
+            street: 'Shevchenko',
+            number: 121
+        }
+    },
+    {
+        id: 4,
+        name: 'olya',
+        age: 28,
+        status: false,
+        address: {
+            city: 'Ternopil',
+            street: 'Shevchenko',
+            number: 90
+        }
+    },
+    {
+        id: 5,
+        name: 'max',
+        age: 30,
+        status: true,
+        address: {
+            city: 'Lviv',
+            street: 'Shevchenko',
+            number: 115
+        }
+    },
+    {
+        id: 6,
+        name: 'anya',
+        age: 31,
+        status: false,
+        address: {
+            city: 'Kyiv',
+            street: 'Shevchenko',
+            number: 2
+        }
+    },
+    {
+        id: 7,
+        name: 'oleg',
+        age: 28,
+        status: false,
+        address: {
+            city: 'Ternopil',
+            street: 'Shevchenko',
+            number: 22
+        }
+    },
+    {
+        id: 8,
+        name: 'andrey',
+        age: 29,
+        status: true,
+        address: {
+            city: 'Lviv',
+            street: 'Shevchenko',
+            number: 43
+        }
+    },
+    {
+        id: 9,
+        name: 'masha',
+        age: 30,
+        status: true,
+        address: {
+            city: 'Kyiv',
+            street: 'Shevchenko',
+            number: 12
+        }
+    },
+    {
+        id: 10,
+        name: 'olya',
+        age: 31,
+        status: false,
+        address: {
+            city: 'Lviv',
+            street: 'Shevchenko',
+            number: 16
+        }
+    },
+    {
+        id: 11,
+        name: 'max',
+        age: 31,
+        status: true,
+        address: {
+            city: 'Ternopil',
+            street: 'Shevchenko',
+            number: 121
+        }
+    }
+];
 // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
-// Данные выводить в документ
+// Данные выводить в документ   
+
+let filterFalse = document.getElementById("filterFalse")
+let ageFilter = document.getElementById("age")
+let liveInKyiv = document.getElementById("liveInKyiv")
+let filter = document.getElementsByClassName("filter")[0]
+
+
+filterFalse.onclick = function () {
+    filter.innerHTML = ""
+    if (filterFalse.checked) {
+        for (const user of usersWithAddress) {
+            if (!user.status) {
+                let div = document.createElement('div')
+                div.innerHTML = JSON.stringify(user)
+                filter.appendChild(div)
+            }
+        }
+    }
+}
+ageFilter.onclick = function () {
+    filter.innerHTML = ""
+    if (ageFilter.checked) {
+        for (const user of usersWithAddress) {
+            if (user.age >= 29) {
+                let div = document.createElement('div')
+                div.innerHTML = JSON.stringify(user)
+                filter.appendChild(div)
+            }
+        }
+    }
+}
+liveInKyiv.onclick = function () {
+    filter.innerHTML = ""
+    if (liveInKyiv.checked) {
+        for (const user of usersWithAddress) {
+            if (user.address.city === "Kyiv") {
+                let div = document.createElement('div')
+                div.innerHTML = JSON.stringify(user)
+                filter.appendChild(div)
+            }
+        }
+    }
+}
